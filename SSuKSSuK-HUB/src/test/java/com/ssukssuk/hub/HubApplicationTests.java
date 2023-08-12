0c57bf8aa732f8791aed66e038f81c010731b4b2
TanHan Lee <lth9312@gmail.com>
2023-08-12 15:12:19 +0900

    Initial commit

---
+++ SSuKSSuK-HUB/src/test/java/com/ssukssuk/hub/HubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssukssuk.hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
