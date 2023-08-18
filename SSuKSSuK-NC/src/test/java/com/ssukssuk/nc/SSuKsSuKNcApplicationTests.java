bcf89e16f2420be5acdd848b733a46352727df15
taehan <lth9312@gmail.com>
2023-08-19 00:23:11 +0900

    BO, NC, OOTD, WS modules Init

---
+++ SSuKSSuK-NC/src/test/java/com/ssukssuk/nc/SSuKsSuKNcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssukssuk.nc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SSuKsSuKNcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ SSukSSuk-OOTD/src/main/java/com/ssukssuk/ootd/OOTDApplication.java
@@ -0,0 +1,40 @@
+package com.ssukssuk.ootd;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class OOTDApplication {
+
+	@Value("${spring.profiles.active}")
+	private String propertyType;
+
+	@Value("${server.port}")
+	private int port;
+
+	@Value("${spring.application.name}")
+	private String serverName;
+
+	private static final Logger logger = LoggerFactory.getLogger(OOTDApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(OOTDApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner runner() {
+		return (a) -> {
+			logger.info("###############################################################");
+			logger.info("##["+serverName+":"+port+" Server ON ~! ] Active Mode is " + propertyType);
+			logger.info("###############################################################");
+		};
+	};
+
+}
